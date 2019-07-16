dbb23e50b35e112f20b9b64288805f3a29cca0a5
Dave Roda <davidcroda@gmail.com>
2019-07-16 12:30:22 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='aws_secrets',
+    version='0.1.0',
+    packages=['aws_secrets'],
+    url='https://github.com/davidcroda/aws_secrets',
+    license='MIT',
+    author='David Roda',
+    author_email='davidcroda@gmail.com',
+    description='Python library for resolving secrets from AWS Secrets Manager',
+    install_requires=[
+        'boto3==1.9.188'
+    ]
+)
